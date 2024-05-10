2ce3d38b8d061fd551a8cbe04141e17b1401d4eb
BoonRealCuaaaaa <99378331+BoonRealCuaaaaa@users.noreply.github.com>
2024-05-10 18:56:07 +0700

    hmm

--- models/adsReport.js
+++ models/adsReport.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      tableName: "adsreports",
+      tableName: "adsReports",
       modelName: "AdsReport",
     }
   );
